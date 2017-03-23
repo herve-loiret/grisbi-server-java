2c03e9fa8d050be725438e0e923c9f7c2cd6f028
Hervé Loiret <herve.loiret@gmail.com>
2017-03-23 14:43:04 +0100

    start to try to use spring restdoc

---
+++ src/test/java/grisbiweb/server/controller/AccountControllerDocumentation.java
@@ -0,0 +1,57 @@
+package grisbiweb.server.controller;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.SneakyThrows;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountControllerDocumentation {
+
+	@Rule
+	public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("");
+
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+				.apply(documentationConfiguration(this.restDocumentation))
+				.alwaysDo(document("{method-name}/{step}/",
+						preprocessRequest(prettyPrint()),
+						preprocessResponse(prettyPrint())))
+				.build();
+	}
+
+	@Test
+	@SneakyThrows
+	public void test() {
+		this.mockMvc.perform(get("/accounts/")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andDo(document("index"));
+	}
+
+}
